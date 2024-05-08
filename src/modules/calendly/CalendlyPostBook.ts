bde33f5fd1ddb525d096dd06375c81e1e651570c
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2024-05-08 17:16:46 +0800

    feat: added-dynamic-chatbase-config

---
+++ src/modules/calendly/CalendlyPostBook.ts
@@ -0,0 +1,12 @@
+export interface CalendlyPostBook {
+  assigned_to: string; // 'Virtual HQ',
+  event_type_uuid: string; // '8091d2b3-6ecf-4d73-b9a8-5704de2b44bd',
+  event_type_name: string; // 'Book with DMCC!',
+  event_start_time: string; // '2024-05-29T15:30:00+08:00',
+  event_end_time: string; // '2024-05-29T16:00:00+08:00',
+  invitee_uuid: string; // '803aa218-2d84-4d58-9983-de14521c8c91',
+  invitee_full_name: string; // 'Test',
+  invitee_email: string; // 'leomarkcastro@gmail.com',
+  guests: string[]; // [ 'leomarkcastro123@gmail.com', 'leomarkcastro.xurpas@gmail.com' ],
+  answer_2: string; // 'Meeting Room'
+}
