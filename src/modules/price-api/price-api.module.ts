5ce1400c47d4c5bb6e9bde94acdfc99991924987
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2024-05-08 11:56:35 +0800

    feat: fix-price-api

--- src/modules/price-api/price-api.module.ts
+++ src/modules/price-api/price-api.module.ts
@@ -1,4 +1,5 @@
 import { NestCacheModule } from '@modules/cache/cache.module';
+import { DbModule } from '@modules/db/db.module';
 import { ServerTokensModule } from '@modules/server-tokens/server-tokens.module';
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
@@ -7,7 +8,7 @@ import { PriceApiCoreHttpService } from './services/price-api-core-http.service'
 import { PriceApiService } from './services/price-api-ticker.service';
 
 @Module({
-  imports: [NestCacheModule, HttpModule, ServerTokensModule],
+  imports: [DbModule, NestCacheModule, HttpModule, ServerTokensModule],
   providers: [PriceApiService, PriceApiCoreHttpService],
   controllers: [PriceApiController],
 })
