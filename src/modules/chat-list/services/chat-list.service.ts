2526f2020281490573d84b3582736c2f5d9c8097
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2024-06-07 15:28:51 +0800

    added-chat-reset

--- src/modules/chat-list/services/chat-list.service.ts
+++ src/modules/chat-list/services/chat-list.service.ts
@@ -38,6 +38,27 @@ export class ChatListService {
     }
   }
 
+  async deleteChatListData(chatID: string) {
+    // if chatHistory exists, update it
+    const chatHistory = await this.db.chatHistory.findFirst({
+      where: {
+        roomID: chatID,
+      },
+    });
+
+    if (chatHistory) {
+      return this.db.chatHistory.delete({
+        where: {
+          id: chatHistory.id,
+        },
+      });
+    }
+  }
+
+  async resetChatList() {
+    return this.db.chatHistory.deleteMany({});
+  }
+
   // get
   async getChatListData(chatID: string) {
     const chatHistory = await this.db.chatHistory.findFirst({
